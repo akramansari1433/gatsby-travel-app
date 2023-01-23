44ae68124bab8953f75e98d0b4525374e6ecf7d3
“akramansari1433” <akramansari1433@gmail.com>
2023-01-23 18:05:22 +0530

    added slice api

---
+++ gatsby-node.js
@@ -0,0 +1,6 @@
+exports.createPages = async ({ actions }) => {
+    actions.createSlice({
+        id: `navbar`,
+        component: require.resolve(`./src/components/Navbar.jsx`),
+    });
+};
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
+import { Slice } from "gatsby";
 import React from "react";
-import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
-   return (
-      <div className="max-w-7xl mx-auto">
-         <Navbar />
-         {children}
-      </div>
-   );
+    return (
+        <div className="max-w-7xl mx-auto">
+            <Slice alias="navbar" />
+            {children}
+        </div>
+    );
 }
